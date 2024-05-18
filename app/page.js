8cf5ecafbb5f9a22543fb7884511a1829c0c9813
viveksakthi <viveksakthicss@gmail.com>
2024-05-18 17:39:15 +0530

    header menu click

--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import Header from "@/components/Header"
 import LeftSidebar from "@/components/LeftSidebar"
+import { useState } from "react";
 
 export default function Home() {
+  
+  const [leftSidebarOpen, setLeftSidebarOpen] = useState(true);
+
   return (
-    <main className="ml-[260px]">
-      <Header />
+    <main className={`${leftSidebarOpen ? "ml-[260px]" : "ml-[0px]"} `}>
+      <Header leftSidebarOpen={leftSidebarOpen} setLeftSidebarOpen={setLeftSidebarOpen} />
       <LeftSidebar />
     </main>
   );
--- components/Header.js
+++ components/Header.js
@@ -2,11 +2,16 @@ import Image from "next/image";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 
-export default function Header() {
+export default function Header({ leftSidebarOpen, setLeftSidebarOpen }) {
   return (
     <header className="text-center px-[24px] py-[14px] flex justify-between">
-      <FontAwesomeIcon icon={faBars} className="w-[24px] h-[24px] cursor-pointer" />
-      
+
+      <FontAwesomeIcon 
+        icon={faBars} 
+        className="w-[24px] h-[24px] cursor-pointer" 
+        onClick={ () => setLeftSidebarOpen(!leftSidebarOpen) }
+      />
+
     </header>
   );
 }
