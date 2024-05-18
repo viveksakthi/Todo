52f58597ccfec344e1b970f6a7729d1fce50457a
viveksakthi <viveksakthicss@gmail.com>
2024-05-18 17:26:09 +0530

    header component added

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+export default function Header() {
+  return (
+    <header className="text-center px-[24px] py-[14px] flex justify-between">
+      <FontAwesomeIcon icon={faBars} className="w-[24px] h-[24px] cursor-pointer" />
+      
+    </header>
+  );
+}
---
+++ components/LeftSidebar.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+
+export default function LeftSidebar() {
+  return (
+    <div className="bg-[#3b3b3b] fixed left-0 top-0 bottom-0 w-[260px]">      
+      
+    </div>
+  );
+}
